1beca87d96b16b91f6c1e906ef85d2ecc15aecc0
Mytarget <liwei5730@126.com>
2016-09-04 15:43:08 +0800

    Project init

---
+++ app/src/main/java/com/betterday/mytarget/TargetListActivity.java
@@ -0,0 +1,13 @@
+package com.betterday.mytarget;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TargetListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_target_list);
+    }
+}
